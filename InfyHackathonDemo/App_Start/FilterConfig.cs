0c3b577b90ebf8f62f9dedbd24b341526d4682cd
mufaddal_gulshan <mufaddal.gulshan@gmail.com>
2016-02-27 01:34:52 +0530

    Initial Commit

---
+++ InfyHackathonDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace InfyHackathonDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ InfyHackathonDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InfyHackathonDemo.Startup))]
+namespace InfyHackathonDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
